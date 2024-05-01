10443c4503b72111cb2ee7e997c4a5fa26dcfef5
Abuhurraira76 <abuhurraira3791@gmail.com>
2024-05-01 16:31:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+return<>
+<div className="w-screen h-screen bg-yellow-400 flex justify-center items-center " >
+  <div className="w-[300px] h-[300px] bg-red-500 flex justify-center items-center rounded-lg">
+    <div className="text-black-500 font-bold ">
+    Hurraira</div>
+  </div>
+</div>
+</>
+}
